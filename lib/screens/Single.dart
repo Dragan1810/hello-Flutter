eaab4d32c609c2f3ee37dc016206bf4aee1b245c
Dragan <d.novakovic1810@yahoo.com>
2018-08-08 18:27:12 +0200

    home screen init

--- lib/screens/Single.dart
+++ lib/screens/Single.dart
@@ -1,31 +1,75 @@
 import 'package:flutter/material.dart';
 
 class SingleView extends StatefulWidget {
-
   @override
   SingleState createState() => SingleState();
 }
 
 class SingleState extends State<SingleView> {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            img(),
+            info(),
+            buttons(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget img() {
+  return Image.asset(
+    'assets/lake.jpg',
+    height: 240.0,
+    width: 600.0,
+    fit: BoxFit.cover,
+  );
+}
+
+Widget info() {
+  return Row(
+    children: <Widget>[
+      Expanded(
+        child: Column(
+          children: <Widget>[
+            Text("Oeschinen Lake Campground"),
+            Text("Kandersteg, Switzerland"),
+          ],
+        ),
+      ),
+      Icon(Icons.star),
+      Text("41")
+    ],
+  );
+}
+
+Widget buttons() {
+  return Row(
+    children: <Widget>[
+      Column(
         children: <Widget>[
-          Text("Hello"),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Text("Hello :P"),
-              ),
-              Icon(Icons.ac_unit),
-              Text("41")
-            ],
-          )
+          Icon(Icons.call),
+          Text("CALL"),
         ],
       ),
-        );
-  }
+      Column(
+        children: <Widget>[
+          Icon(Icons.directions_run),
+          Text("ROUTE"),
+        ],
+      ),
+      Column(
+        children: <Widget>[
+          Icon(Icons.share),
+          Text("SHARE"),
+        ],
+      ),
+    ],
+  );
 }
