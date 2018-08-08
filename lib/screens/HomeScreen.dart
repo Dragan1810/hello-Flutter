b1f425530efae77fd00fb19e2045b209269e0da2
Dragan <d.novakovic1810@yahoo.com>
2018-08-08 13:00:36 +0200

    home screen init

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -5,6 +5,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HomeScreen"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                GestureDetector (
+                  onTap: () {
+                    print("onTap called.");
+                  },
+                  child: Container(
+                  decoration: new BoxDecoration(
+                      border: new Border.all(color: Colors.blueAccent)
+                  ),
+                    child: Text("Single KLIK"),
+                    width: 100.00,
+                    height: 100.00,
+                ),
+                ),
+                Container(
+                  decoration: new BoxDecoration(
+                      border: new Border.all(color: Colors.blueAccent)
+                  ),
+                  child: Text("RoW"),
+                  width: 100.00,
+                  height: 100.00,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  decoration: new BoxDecoration(
+                    border: new Border.all(color: Colors.blueAccent)
+                  ),
+                  child: Text("RoW"),
+                  width: 100.00,
+                  height: 100.00,
+                ),
+                Container(
+                  decoration: new BoxDecoration(
+                      border: new Border.all(color: Colors.blueAccent)
+                  ),
+                  child: Text("RoW"),
+                  width: 100.00,
+                  height: 100.00,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
