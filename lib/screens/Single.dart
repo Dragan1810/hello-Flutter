a1aaea7887e18d4f2d41ae6534731d70f4a90538
Dragan <d.novakovic1810@yahoo.com>
2018-08-09 09:46:22 +0200

    Single Screen demo

--- lib/screens/Single.dart
+++ lib/screens/Single.dart
@@ -10,12 +10,12 @@ class SingleState extends State<SingleView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(10.0),
         child: Column(
           children: <Widget>[
             img(),
             info(),
             buttons(),
+            textSection,
           ],
         ),
       ),
@@ -33,24 +33,40 @@ Widget img() {
 }
 
 Widget info() {
-  return Row(
-    children: <Widget>[
-      Expanded(
-        child: Column(
-          children: <Widget>[
-            Text("Oeschinen Lake Campground"),
-            Text("Kandersteg, Switzerland"),
-          ],
+  return Padding(
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                  "Oeschinen Lake Campground",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Kandersteg, Switzerland",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      Icon(Icons.star),
-      Text("41")
-    ],
+        Icon(
+          Icons.star,
+          color: Colors.red,
+        ),
+        Text("41")
+      ],
+    ),
   );
 }
 
 Widget buttons() {
   return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: <Widget>[
       Column(
         children: <Widget>[
@@ -73,3 +89,13 @@ Widget buttons() {
     ],
   );
 }
+
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32.0),
+  child: Text(
+    '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+    softWrap: true,
+  ),
+);
