722f5478a16fd9e4ceb101f1a3a704a1a5ae6ca4
Dragan <d.novakovic1810@yahoo.com>
2018-08-11 12:48:53 +0200

    GridList bug

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -2,70 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:hello_flutter/screens/Single.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("HomeScreen"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                InkWell (
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SingleView()),
-                    );
-                  },
-                  child: Container(
-                  decoration: new BoxDecoration(
-                      border: new Border.all(color: Colors.blueAccent)
-                  ),
-                    child: Text("Single KLIK"),
-                    width: 100.00,
-                    height: 100.00,
-                ),
-                ),
-                Container(
-                  decoration: new BoxDecoration(
-                      border: new Border.all(color: Colors.blueAccent)
-                  ),
-                  child: Text("RoW"),
-                  width: 100.00,
-                  height: 100.00,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  decoration: new BoxDecoration(
-                    border: new Border.all(color: Colors.blueAccent)
-                  ),
-                  child: Text("RoW"),
-                  width: 100.00,
-                  height: 100.00,
-                ),
-                Container(
-                  decoration: new BoxDecoration(
-                      border: new Border.all(color: Colors.blueAccent)
-                  ),
-                  child: Text("RoW"),
-                  width: 100.00,
-                  height: 100.00,
-                ),
-              ],
-            ),
-          ],
+      bottomNavigationBar: BottomNavigationBar(items: [
+        bottomNavItem(),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          title: Text("Home"),
         ),
-      ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          title: Text("Home"),
+        )
+      ]),
+      body: Container(
+        padding: EdgeInsets.all(0.0),
+        child: Stack(
+          children: <Widget>[
+            Image.asset('./assets/lake.jpg',height: 320.0,fit: BoxFit.fill),
+            Column(
+              children: <Widget>[
+              Text("Velik Naslov Preko Cele Sike, A ispod je Grid Meni"),
+              Expanded(
+                child: GridView.count(
+                    primary: true,
+                    padding: EdgeInsets.all(20.0),
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      Text('He\'d have you all unravel at the'),
+                      Text('Heed not the rabble'),
+                      Text('Sound of screams but the'),
+                      Text('Who scream'),
+                      Text('Revolution is coming...'),
+                      Text('Revolution, they...'),
+                    ],
+                  ),
+              )
+            ],
+            )
+              ],
+            ),
+        ),
     );
   }
+}
+
+bottomNavItem() {
+  return BottomNavigationBarItem(
+    icon: Icon(Icons.home),
+    title: Text("Home"),
+  );
 }
